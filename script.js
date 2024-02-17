c7e2e18a93443b51a2553a9c525226871ed08f80
kanya <tripathikanyam2262@gmail.com>
2024-02-17 22:56:34 +0545

    updated

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const taskInput = document.getElementById("taskInput");
+const  addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+
+
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+
+let value = localStorage.getItem("tasks");
+console.log(typeof value);
+
+
+addTaskBtn.addEventListener("click", function () {
+    const taskText = taskInput.value.trim();
+    if (taskText !== ""){
+        addTaskToList(taskText);
+        tasks.push(taskText);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        taskInput.value = "";
+    }
+});
+
+function addTaskToList(taskText) {
+    const taskItem = document.createElement("li");
+    taskItem.classList.add("taskItem");
+    taskItem.textContent = taskText;
+
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent="Delete";
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.addEventListener("click", function() {
+        const index = tasks.indexOf(taskText);
+        if (index !== -1) {
+            tasks.splice(index, 1);
+            localStorage.setItem("tasks",JSON.stringify(tasks))
+            taskItem.remove( );
+            }
+        });
+
+
+
+        taskItem.appendChild(deleteBtn);
+    taskList.appendChild(taskItem);
+
+    }
+
